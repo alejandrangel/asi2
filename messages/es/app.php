0c5b0dad3ebba46f1ae7e70e8af6b1f8e011a804
alexcotte <alexanortega@gmail.com>
2016-11-09 23:16:32 -0600

    initial commit

---
+++ messages/es/app.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 5/9/16
+ * Time: 9:39 PM
+ */
+
+return [
+    'Showing' => 'Mostrando',
+    'item' => 'item',
+    'items' => 'items',
+    'of' => 'de',
+    'create'=>'crear',
+	'Activities'=>'Actividades',
+	'Team'=>'Equipo',
+	'New Activity' => 'Nueva Actividad',
+    'Tool' =>'Herramienta',
+    'Tools' =>'Herramientas',
+    'New Tool' =>'Agregar Herramienta',
+    'New Automotor' => 'Agregar Automotor',
+    'New Personal' => 'Agregar Personal'
+];
