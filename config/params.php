27d6c607b0ee951805b6307560caaf5504cfd97a
alexcotte <alexanortega@gmail.com>
2016-11-29 22:12:27 -0600

    pequeños cambios a equipo

--- config/params.php
+++ config/params.php
@@ -3,7 +3,7 @@
 return [
     'adminEmail' => 'admin@example.com',
     'empresa'=>'Alcaldia Municipal de Santa Tecla',
-    'sys-name'=>'Deshechos solidos.',
+    'sys-name'=>'Sistema de Gestión de Solicitudes y Rutas de Recolección de Desechos Solidos',
     'logo'=>'http://santatecla.gob.sv/images/logo-blanco.png',
     'logo-report'=>'http://santatecla.gob.sv/images/logo-color.png'
 ];
