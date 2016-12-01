858e181d660adda0b9fa200ca54a233e06f8ee3e
alexcotte <alexanortega@gmail.com>
2016-11-30 23:44:47 -0600

    cosas de los equipos

--- util/Log.php
+++ util/Log.php
@@ -10,15 +10,19 @@ class Log{
 	
 	const LOGIN  = 'Entro al sistema';
 	const LOGOUT = 'Salio del sistema';
+
 	const AC_MODULE_CARGO  = 'Ingreso a Mantenimiento de cargos';
 	
 	
 	public static function log($accion = null){
-		
-		$userId = Yii::$app->user->id;
-		$log = new LogUser();
-		$log->usuario = $userId;
-		$log->accion  = $accion;
-		$log->save();
+	    try{
+            $userId = Yii::$app->user->id;
+            $log = new LogUser();
+            $log->usuario = $userId;
+            $log->accion  = $accion;
+            $log->save();
+        }catch (yii\db\Exception $e){
+
+        }
 	}
 }
