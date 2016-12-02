221270407ec26f24587fa51eed4fd6a4a5538cc0
alejandrangel <alemaria_angel@hotmail.com>
2016-12-01 22:46:55 -0600

    Update index.php

--- views/estado/index.php
+++ views/estado/index.php
@@ -18,7 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Estado', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Crear Estados', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
